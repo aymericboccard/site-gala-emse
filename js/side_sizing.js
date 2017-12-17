d2de5deb7904547947a3c30e4dad40ca5ebe0911
aymericboccard <aymeric.boccard@gmail.com>
2017-12-17 15:20:08 +0100

    fix: erase alert in side_sizing

--- js/side_sizing.js
+++ js/side_sizing.js
@@ -1,9 +1,7 @@
 var size = document.getElementById('content').clientHeight;
-alert('ok');
 if(size > 235) {
   document.getElementById('side').style.height = size+"px";
 } else {
-  alert((235-size)+"px");
   document.getElementById('content').style.paddingBottom = (235-size)+"px";
 
 }
