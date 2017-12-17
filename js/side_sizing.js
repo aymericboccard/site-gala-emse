ccf80853e63e9306a03bef98d6b4128f08ec3156
aymericboccard <aymeric.boccard@gmail.com>
2017-12-17 14:40:01 +0100

    feat: adaptative sidebar

--- js/side_sizing.js
+++ js/side_sizing.js
@@ -1,5 +1,9 @@
 var size = document.getElementById('content').clientHeight;
-alert(size);
+alert('ok');
 if(size > 235) {
-  document.getElementById('side').clientHeight = size+"px";
+  document.getElementById('side').style.height = size+"px";
+} else {
+  alert((235-size)+"px");
+  document.getElementById('content').style.paddingBottom = (235-size)+"px";
+
 }
