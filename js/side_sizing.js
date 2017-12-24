ad31bdc8324e62aab0d77ad6f9c461c727a4de20
Aymeric Boccard <aymeric.boccard@gmail.com>
2017-12-24 15:01:13 +0100

    fix: non clickable button in sidebar

--- js/side_sizing.js
+++ js/side_sizing.js
@@ -1,7 +1,8 @@
-var size = document.getElementById('content').clientHeight;
-if(size > 235) {
-  document.getElementById('side').style.height = size+"px";
-} else {
-  document.getElementById('content').style.paddingBottom = (235-size)+"px";
-
+function resize() {
+  var size = document.getElementById('content').clientHeight;
+  if(size > 235) {
+    document.getElementById('side').style.height = size+"px";
+  } else {
+    document.getElementById('content').style.paddingBottom = (235-size)+"px";
+  }
 }
