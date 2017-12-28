4fc2a4ea4078e37008082110b2535ed283be9079
Aymeric Boccard <aymeric.boccard@gmail.com>
2017-12-28 13:04:21 +0100

    fix bug autosize

--- js/side_sizing_auto.js
+++ js/side_sizing_auto.js
@@ -1,12 +1,11 @@
 function resize() {
   var sizediff = document.getElementById('banner').clientHeight;
-  var footer = document.getElementById('footer');
   var elements = document.getElementsByTagName('body');
-  var size = elements[0].clientHeight-sizediff+50;
+  var size = elements[0].clientHeight-sizediff+100;
   if(size > 232) {
-    document.getElementById('side').style.height = size+100+"px";
+    document.getElementById('side').style.height = size+"px";
   } else {
     document.getElementById('content').style.height = 232+"px";
   }
-  footer.style.position = "absolute";
 }
+ setInterval(resize, 2000);
